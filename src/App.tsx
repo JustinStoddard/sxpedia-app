7ebfc50615a3749523e8cf4b97980e11de1d67fd
Justin Stoddard <justinstoddard@Justins-MacBook-Pro.local>
2021-08-30 13:47:21 -0600

    added some pages and private route hoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import { PrivateRoute } from './Components/HOC/PrivateRoute';
+import LandingPage from './Pages/LandingPage';
+import LoginPage from './Pages/LoginPage';
+import SignUpPage from './Pages/SignUpPage';
+import HomePage from './Pages/HomePage';
+import NotFoundPage from './Pages/NotFoundPage';
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/welcome' component={LandingPage} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/signup' component={SignUpPage} />
+          <Route exact path='/404' component={NotFoundPage} />
+          <Route path="*">
+            <Redirect to="/404" />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 };
 
---
+++ src/Components/HOC/PrivateRoute/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
+/**
+ * HOC for making sure a user is authorized for using a route
+ * @Returns Authenticated Route
+ */ 
+export const PrivateRoute = ({ component: Component, ...rest }: any) => {
+  const { user } = useAuth0();
+ 
+  if (user) return <Route {...rest} component={(props: any) => <Component {...props} />} />
+  return <Redirect to="/" />
+};
